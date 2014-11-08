3c08887db92aa502e4e0a32b8cd20b72bfc615fb
Daniel Buckmaster <dan.buckmaster@gmail.com>
2014-11-08 13:12:05 +1100

    Vagrant setup.

---
+++ vm/.bash_profile
@@ -0,0 +1,4 @@
+export PATH=/opt/ghc/7.8.3/bin:/opt/cabal/1.20/bin:$PATH
+
+# Go to working directory on login
+cd /vagrant
---
+++ vm/provision.sh
@@ -0,0 +1,17 @@
+# Copy profile into home directory
+cd /vagrant/vm
+cp .bash_profile $HOME/.bash_profile
+
+# Install Haskell tools
+echo "Installing GHC and cabal-install"
+apt-add-repository "deb http://ppa.launchpad.net/hvr/ghc/ubuntu trusty main"
+apt-get update
+apt-get install -y --force-yes ghc-7.8.3 cabal-install-1.20
+
+# Install Heroku tools
+if hash heroku 2>/dev/null; then
+	echo "Heroku already installed :)"
+else
+	echo "Installing Heroku"
+	wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+fi
